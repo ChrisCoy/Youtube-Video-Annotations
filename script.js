838ace22bdaf5543a734734e706201087a75be08
Christopher Lee <christopherlee.contato@gmail.com>
2022-01-15 17:39:42 -0300

    create base modal

--- script.js
+++ script.js
@@ -17,61 +17,77 @@
 //   }
 // });
 
-// window.addEventListener("load", function () {
-//   alert("carregou :D");
-// });
-let divContent;
+let divContent; //div from youtube, theres will gotta something just when the page is full loaded
 let divContentInterval;
-let vid;
-let divTextArea;
-let txtArea;
+const htmlBody = document.getElementsByTagName("body")[0];
+
+let blackBackScreen;
+let video;
 let url;
+
+let divExtensionContent;
+
+function closeWindow() {
+  blackBackScreen.remove();
+  divExtensionContent.remove();
+  console.log("TEST");
+}
 
 function Save() {
   localStorage.setItem(url, String(txtArea.value));
-  console.log(txtArea.value);
-  divTextArea.remove();
 }
 
 function btnAction() {
-  if (!document.getElementById("textWindow")) {
-    url = window.location.href.split("=")[1].split("&")[0];
-    vid = document.querySelector(".video-stream.html5-main-video");
-    vid.pause();
+  url = window.location.href.split("=")[1].split("&")[0];
+  video = document.querySelector(".video-stream.html5-main-video");
+  video.pause();
 
-    divTextArea = document.createElement("div");
-    divTextArea.id = "divTextArea";
+  //creating the black screen that will be on the top of everything
+  blackBackScreen = document.createElement("button");
+  blackBackScreen.className = "black-back-screen";
+  blackBackScreen.onclick = closeWindow;
+  htmlBody.appendChild(blackBackScreen);
 
-    let btnSave = document.createElement("button");
-    btnSave.id = "btnSave";
-    btnSave.onclick = Save;
-    btnSave.innerText = "Salvar";
+  //extension content div
+  divExtensionContent = document.createElement("div");
+  divExtensionContent.className = "extension-content";
+  divExtensionContent.id = "extension-content";
 
-    let btnCancel = document.createElement("button");
-    btnCancel.id = "btnCancel";
-    btnCancel.onclick = () => divTextArea.remove();
-    btnCancel.innerText = "Fechar";
-
-    txtArea = document.createElement("textarea");
-    txtArea.id = "textWindow";
-    txtArea.value = localStorage.getItem(url);
-
-    divTextArea.append(txtArea);
-    divTextArea.append(btnSave);
-    divTextArea.append(btnCancel);
-
-    divContent.append(divTextArea);
-    console.log(txtArea);
-  }
+  htmlBody.appendChild(divExtensionContent);
 }
 
 function loadDiv() {
   divContent = document.querySelector(".style-scope ytd-video-primary-info-renderer #info");
   if (divContent) {
-    let btn = document.createElement("button");
-    btn.innerText = "Adicionar Nota";
-    btn.onclick = btnAction;
-    divContent.appendChild(btn);
+    const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    iconSvg.setAttributeNS("", "height", "21");
+    iconSvg.setAttributeNS("", "width", "20");
+    iconSvg.setAttributeNS("", "viewBox", "0 0 24 24");
+    iconSvg.setAttributeNS("", "fill", "#FFFFFF");
+
+    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    iconPath.setAttributeNS(
+      "",
+      "d",
+      "m16 16a1 1 0 0 1 -1 1h-2v2a1 1 0 0 1 -2 0v-2h-2a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2h2a1 1 0 0 1 1 1zm6-5.515v8.515a5.006 5.006 0 0 1 -5 5h-10a5.006 5.006 0 0 1 -5-5v-14a5.006 5.006 0 0 1 5-5h4.515a6.958 6.958 0 0 1 4.95 2.05l3.484 3.486a6.951 6.951 0 0 1 2.051 4.949zm-6.949-7.021a5.01 5.01 0 0 0 -1.051-.78v4.316a1 1 0 0 0 1 1h4.316a4.983 4.983 0 0 0 -.781-1.05zm4.949 7.021c0-.165-.032-.323-.047-.485h-4.953a3 3 0 0 1 -3-3v-4.953c-.162-.015-.321-.047-.485-.047h-4.515a3 3 0 0 0 -3 3v14a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3z"
+    );
+
+    iconSvg.appendChild(iconPath);
+
+    console.log(iconSvg);
+
+    const divButton = document.createElement("div");
+    divButton.onclick = btnAction;
+    divButton.className = "button-menu";
+
+    divButton.appendChild(iconSvg);
+
+    const spanTextTitle = document.createElement("span");
+    spanTextTitle.innerText = "NEW ANNOTATION";
+
+    divButton.appendChild(spanTextTitle);
+    divContent.appendChild(divButton);
     clearInterval(divContentInterval);
   }
 }
